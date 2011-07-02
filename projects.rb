2b5371afeadd05a24270c4ee629ac7a09246f53c
Nat Welch <nat@natwelch.com>
2011-07-03 00:45:48 +0100

    This does it for the most part.

--- projects.rb
+++ projects.rb
@@ -1,1 +1,27 @@
+#!/usr/bin/env ruby
 
+require 'time'
+require 'date'
+require 'octokit' # gem install octokit
+require 'terminal-table/import' # gem install terminal-table
+
+
+print "Enter your github username: "
+user = gets.chomp
+
+print "Enter your github password: "
+token = gets.chomp
+
+client = Octokit::Client.new(:login => user, :password => token)
+
+puts "#{user}'s GitHub repos:"
+
+
+ascii_table = table do |t|
+   t.headings = ["Project Name", "Date", "Description"]
+   client.repos(user).each do |repo|
+      t << [ repo.name,Date.parse(repo.created_at).to_s, repo.description]
+   end
+end
+
+puts ascii_table
