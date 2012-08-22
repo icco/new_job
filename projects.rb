5be825b9870ffb1ba1f4297a85c1fe428ca96942
Nat Welch <nat@natwelch.com>
2012-08-22 08:19:12 -0700

    adds option parsing and new list format.

--- projects.rb
+++ projects.rb
@@ -8,6 +8,29 @@ Bundler.require
 require 'time'
 require 'date'
 require 'io/console'
+require 'optparse'
+
+options = {}
+
+optparse = OptionParser.new do|opts|
+  # Set a banner, displayed at the top
+  # of the help screen.
+  opts.banner = "Usage: projects.rb [options]"
+
+  options[:display] = "table"
+  opts.on( '-d', '--display {table,list}', 'Define display type.' ) do |type|
+    options[:display] = type
+  end
+
+  # This displays the help screen, all programs are
+  # assumed to have this option.
+  opts.on( '-h', '--help', 'Display this screen' ) do
+    puts opts
+    exit
+  end
+end
+
+optparse.parse!
 
 print "Enter your github username: "
 user = gets.chomp
@@ -20,13 +43,30 @@ client = Octokit::Client.new(:login => user, :password => token.strip, :auto_tra
 puts ""
 puts "#{user}'s GitHub repos:"
 
-ascii_table = table do |t|
-  t.headings = ["Project Name", "Date", "Description"]
-  client.repos(user).each do |repo|
-    if !repo.fork?
-      t << [ repo.name,Date.parse(repo.created_at).to_s, repo.description]
+if options[:display] == "table"
+  ascii_table = table do |t|
+    t.headings = ["Project Name", "Date", "Description"]
+    client.repos(user).each do |repo|
+      if !repo.fork?
+        t << [ repo.name, Date.parse(repo.created_at).to_s, repo.description ]
+      end
     end
   end
-end
 
-puts ascii_table
+  puts ascii_table
+elsif options[:display] == "list"
+  client.repos(user).each do |repo|
+    if !repo.fork?
+      puts " * #{repo.name} - #{Date.parse(repo.created_at).to_s}"
+      puts "   * #{repo.homepage}" if !repo.homepage.nil? && !repo.homepage.empty?
+      puts "   * #{repo.html_url}" if !repo.private
+      puts "   * #{repo.description}" if !repo.description.nil? && !repo.description.empty?
+      puts ""
+    end
+  end
+elsif options[:display] == "dump"
+  client.repos(user).each do |repo|
+    p repo if !repo.fork?
+    puts ""
+  end
+end
