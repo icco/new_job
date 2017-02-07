13732713989ae633c58e3a2f0522c0599819f2d0
Nat Welch <nat@natwelch.com>
2017-02-07 05:16:47 +0000

    rubocop -a

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
-source 'https://rubygems.org'
+source "https://rubygems.org"
 
 ruby "2.4.0"
 
-gem "io-console", :require => "io/console"
+gem "io-console", require: "io/console"
 gem "netrc"
 gem "octokit"
 gem "terminal-table"
--- projects.rb
+++ projects.rb
@@ -1,13 +1,13 @@
 #! /usr/bin/env ruby
 
-require 'rubygems'
-require 'bundler'
+require "rubygems"
+require "bundler"
 
 Bundler.require
 
-require 'date'
-require 'optparse'
-require 'time'
+require "date"
+require "optparse"
+require "time"
 
 # Argument parsing
 options = {}
@@ -15,17 +15,17 @@ OptionParser.new do |opts|
   opts.banner = "Usage: projects.rb [options]"
 
   options[:display] = "table"
-  opts.on( '-d', '--display {table,list,columns}', "Define display type. Default: #{options[:display]}" ) do |type|
+  opts.on("-d", "--display {table,list,columns}", "Define display type. Default: #{options[:display]}") do |type|
     options[:display] = type
   end
 
   options[:sort] = "alpha"
-  opts.on( '-s', '--sort {alpha,date}', "Define sort type. Default: #{options[:sort]}" ) do |type|
+  opts.on("-s", "--sort {alpha,date}", "Define sort type. Default: #{options[:sort]}") do |type|
     options[:sort] = type
   end
 
   options[:token] = nil
-  opts.on( '-t', '--token <40 char auth token>', 'User\'s auth token.' ) do |user|
+  opts.on("-t", "--token <40 char auth token>", 'User\'s auth token.') do |user|
     options[:token] = user
   end
 
@@ -34,7 +34,7 @@ OptionParser.new do |opts|
     options[:netrc] = netrc
   end
 
-  opts.on( '-h', '--help', 'Display this screen' ) do
+  opts.on("-h", "--help", "Display this screen") do
     puts opts
     exit
   end
@@ -43,7 +43,7 @@ end.parse!
 Octokit.auto_paginate = true
 
 if options[:netrc]
-  client = Octokit::Client.new(:netrc => true)
+  client = Octokit::Client.new(netrc: true)
 else
   if options[:token].nil?
     $stderr.print "Enter your github auth token: "
@@ -52,16 +52,16 @@ else
     token = options[:token]
   end
 
-  client = Octokit::Client.new(:access_token => token)
+  client = Octokit::Client.new(access_token: token)
 end
 
 puts "#{client.login}'s GitHub repos:"
 
 repos = []
 if options[:sort] == "alpha"
-  repos = client.repos(client.login, :sort => 'full_name')
+  repos = client.repos(client.login, sort: "full_name")
 elsif options[:sort] == "date"
-  repos = client.repos(client.login, :sort => 'created')
+  repos = client.repos(client.login, sort: "created")
 end
 
 case options[:display]
@@ -69,8 +69,8 @@ when "table"
   ascii_table = Terminal::Table.new do |t|
     t.headings = ["Project Name", "Date", "Description"]
     repos.each do |repo|
-      if !repo.fork?
-        t << [ repo.name, repo.created_at.strftime('%F'), repo.description ]
+      unless repo.fork?
+        t << [repo.name, repo.created_at.strftime("%F"), repo.description]
       end
     end
   end
@@ -78,27 +78,26 @@ when "table"
   puts ascii_table
 when "list"
   repos.each do |repo|
-    if !repo.fork?
-      puts " * #{repo.name} - #{repo.created_at.strftime('%F')}"
-      puts "   * #{repo.homepage}"    if !repo.homepage.nil? && !repo.homepage.empty?
-      if !repo.private
-        if not repo.html_url.nil?
-          puts "   * #{repo.html_url}"
-        else
-          puts "   * http://github.com/#{client.login}/#{repo.name}"
-        end
+    next if repo.fork?
+    puts " * #{repo.name} - #{repo.created_at.strftime("%F")}"
+    puts "   * #{repo.homepage}" if !repo.homepage.nil? && !repo.homepage.empty?
+    unless repo.private
+      if !repo.html_url.nil?
+        puts "   * #{repo.html_url}"
+      else
+        puts "   * http://github.com/#{client.login}/#{repo.name}"
       end
-      puts "   * #{repo.description}" if !repo.description.nil? && !repo.description.empty?
-      puts ""
     end
+    puts "   * #{repo.description}" if !repo.description.nil? && !repo.description.empty?
+    puts ""
   end
 when "dump"
   repos.each do |repo|
-    p repo if !repo.fork?
+    p repo unless repo.fork?
     puts ""
   end
 when "columns"
   repos.each do |repo|
-    print " #{repo.name},#{repo.created_at.strftime('%F')},#{repo.html_url} |"
+    print " #{repo.name},#{repo.created_at.strftime("%F")},#{repo.html_url} |"
   end
 end
